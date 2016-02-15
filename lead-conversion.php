81ff7458dff03eef5202aa830879477df0ddab54
filipenasc <filipemateusn@gmail.com>
2016-02-15 14:33:54 -0200

    using method instead attribute on CF7

--- lead-conversion.php
+++ lead-conversion.php
@@ -90,7 +90,7 @@ class LeadConversion {
     $forms = $this->get_forms('rdcf7_integrations');
     foreach ($forms as $form) {
       $form_id = get_post_meta($form->ID, 'form_id', true);
-      if ( $form_id == $cf7->id ) {
+      if ( $form_id == $cf7->id() ) {
         $submission = WPCF7_Submission::get_instance();
         if ( $submission ) $this->form_data = $submission->get_posted_data();
         $this->generate_static_fields($form->ID, 'Plugin Contact Form 7');
